456b2be8b703d8b96775a47f0b67eb67fc5ab06c
Paurush Sinha <paurush.sinha.d@gmail.com>
2023-04-18 19:17:29 +0530

    Bugs Fixed

--- pages/comp/result.js
+++ pages/comp/result.js
@@ -5,7 +5,7 @@ import style from '../css/result.module.css';
 export default function Result({link, title, snap}) {
   return (
     <div className={style.search_result}>
-        <Link href={link} className={style.search_result_a}>
+        <Link href={`${link}`} className={style.search_result_a}>
             <span>{title}</span>
         </Link>
         <p className={style.search_result_p}>{snap}</p>
